7e0a3ce7f810c973ad6156b3cd8ff9bd581a0b70
nmy2511 <nmy2511@gmail.com>
2020-05-04 14:43:20 +0900

    github-jenkins hook test

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       </header> */}
       <ButtonToolbar>
           {/* Standard button */}
-          <Button>Default</Button>
+          <Button>Github-jenkins hook test</Button>
           {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
           <Button variant="primary">Primary</Button>
           {/* Indicates a successful or positive action */}
