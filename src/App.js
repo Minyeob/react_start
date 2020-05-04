3fa5f8544d34861b01dfdfe2968225ceb88bd405
nmy2511 <nmy2511@gmail.com>
2020-05-04 14:09:24 +0900

    Add React-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ButtonToolbar, Button } from 'react-bootstrap'; 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +20,23 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <ButtonToolbar>
+          {/* Standard button */}
+          <Button>Default</Button>
+          {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+          <Button variant="primary">Primary</Button>
+          {/* Indicates a successful or positive action */}
+          <Button variant="success">Success</Button>
+          {/* Contextual button for informational alert messages */}
+          <Button variant="info">Info</Button>
+          {/* Indicates caution should be taken with this action */}
+          <Button variant="warning">Warning</Button>
+          {/* Indicates a dangerous or potentially negative action */}
+          <Button variant="danger">Danger</Button>
+          {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+          <Button variant="link">Link</Button>
+      </ButtonToolbar>
     </div>
   );
 }
