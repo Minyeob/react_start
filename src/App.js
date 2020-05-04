d99a6dccd43f21f995ea1859a4ee2c3b18f0aa84
nmy2511 <nmy2511@gmail.com>
2020-05-04 17:50:55 +0900

    retest1

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       </header> */}
       <ButtonToolbar>
           {/* Standard button */}
-          <Button>Github-jenkins hook retest</Button>
+          <Button>Github-jenkins hook retest12</Button>
           {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
           <Button variant="primary">Primary</Button>
           {/* Indicates a successful or positive action */}
